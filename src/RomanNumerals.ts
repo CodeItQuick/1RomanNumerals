1774861eae58d229e273edc400ed5a288e0b6a4b
Evan Theriault <ETheriault@mlock.com>
2024-01-29 14:32:14 -0600

    respacing

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -1,36 +1,36 @@
 export class RomanNumerals {
-  public convert(num) {
-    let result = '';
+    public convert(num) {
+        let result = '';
 
-    let tensPlace = Math.floor(num / 10);
+        let tensPlace = Math.floor(num / 10);
    
-     if (tensPlace >= 2) {
-       num -= 20
-       result += 'XX'
-     } else if (tensPlace >= 1) {
-       num -= 10
-       result += 'X'
-     }
-    
-    if (num === 9) {
-      num -= 9
-      result += 'IX'
-    }
+        if (tensPlace >= 2) {
+            num -= 20
+            result += 'XX'
+        } else if (tensPlace >= 1) {
+            num -= 10
+            result += 'X'
+        }
 
-    if (num >= 5) {
-      num -= 5
-      result += 'V'
-    }
+        if (num === 9) {
+            num -= 9
+            result += 'IX'
+        }
 
-    if (num === 4) {
-      num -= 4
-      result += 'IV'
-    }
+        if (num >= 5) {
+            num -= 5
+            result += 'V'
+        }
 
-    for (let i = 0; i < num; i++) {
-      result += 'I';
+        if (num === 4) {
+            num -= 4
+            result += 'IV'
+        }
+
+        for (let i = 0; i < num; i++) {
+            result += 'I';
+        }
+
+        return result;
     }
- 
-    return result;
-  }
 }
